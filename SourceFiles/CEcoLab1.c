bd7b833971310df39eb0286083744d449dd03bb8
Kirill Lartsev <lartsev200@yandex.ru>
2025-10-07 16:05:47 +0300

    first commit

---
+++ SourceFiles/CEcoLab1.c
@@ -0,0 +1,459 @@
+﻿/*
+ * <кодировка символов>
+ *   Cyrillic (UTF-8 with signature) - Codepage 65001
+ * </кодировка символов>
+ *
+ * <сводка>
+ *   CEcoLab1
+ * </сводка>
+ *
+ * <описание>
+ *   Данный исходный код описывает реализацию интерфейсов CEcoLab1
+ * </описание>
+ *
+ * <автор>
+ *   Copyright (c) 2018 Vladimir Bashev. All rights reserved.
+ * </автор>
+ *
+ */
+
+#include "IEcoSystem1.h"
+#include "IEcoInterfaceBus1.h"
+#include "IEcoInterfaceBus1MemExt.h"
+#include "CEcoLab1.h"
+
+/*
+ *
+ * <сводка>
+ *   Функция QueryInterface
+ * </сводка>
+ *
+ * <описание>
+ *   Функция QueryInterface для интерфейса IEcoLab1
+ * </описание>
+ *
+ */
+static int16_t ECOCALLMETHOD CEcoLab1_QueryInterface(/* in */ IEcoLab1Ptr_t me, /* in */ const UGUID* riid, /* out */ void** ppv) {
+    CEcoLab1* pCMe = (CEcoLab1*)me;
+
+    /* Проверка указателей */
+    if (me == 0 || ppv == 0) {
+        return ERR_ECO_POINTER;
+    }
+
+    /* Проверка и получение запрошенного интерфейса */
+    if ( IsEqualUGUID(riid, &IID_IEcoLab1) ) {
+        *ppv = &pCMe->m_pVTblIEcoLab1;
+        pCMe->m_pVTblIEcoLab1->AddRef((IEcoLab1*)pCMe);
+    }
+    else if ( IsEqualUGUID(riid, &IID_IEcoUnknown) ) {
+        *ppv = &pCMe->m_pVTblIEcoLab1;
+        pCMe->m_pVTblIEcoLab1->AddRef((IEcoLab1*)pCMe);
+    }
+    else {
+        *ppv = 0;
+        return ERR_ECO_NOINTERFACE;
+    }
+    return ERR_ECO_SUCCESES;
+}
+
+/*
+ *
+ * <сводка>
+ *   Функция AddRef
+ * </сводка>
+ *
+ * <описание>
+ *   Функция AddRef для интерфейса IEcoLab1
+ * </описание>
+ *
+ */
+static uint32_t ECOCALLMETHOD CEcoLab1_AddRef(/* in */ IEcoLab1Ptr_t me) {
+    CEcoLab1* pCMe = (CEcoLab1*)me;
+
+    /* Проверка указателя */
+    if (me == 0 ) {
+        return -1; /* ERR_ECO_POINTER */
+    }
+
+    return ++pCMe->m_cRef;
+}
+
+/*
+ *
+ * <сводка>
+ *   Функция Release
+ * </сводка>
+ *
+ * <описание>
+ *   Функция Release для интерфейса IEcoLab1
+ * </описание>
+ *
+ */
+static uint32_t ECOCALLMETHOD CEcoLab1_Release(/* in */ IEcoLab1Ptr_t me) {
+    CEcoLab1* pCMe = (CEcoLab1*)me;
+
+    /* Проверка указателя */
+    if (me == 0 ) {
+        return -1; /* ERR_ECO_POINTER */
+    }
+
+    /* Уменьшение счетчика ссылок на компонент */
+    --pCMe->m_cRef;
+
+    /* В случае обнуления счетчика, освобождение данных экземпляра */
+    if ( pCMe->m_cRef == 0 ) {
+        deleteCEcoLab1((IEcoLab1*)pCMe);
+        return 0;
+    }
+    return pCMe->m_cRef;
+}
+
+/*
+ *
+ * <сводка>
+ *   Функция Init
+ * </сводка>
+ *
+ * <описание>
+ *   Функция инициализации экземпляра
+ * </описание>
+ *
+ */
+int16_t ECOCALLMETHOD initCEcoLab1(/*in*/ IEcoLab1Ptr_t me, /* in */ struct IEcoUnknown *pIUnkSystem) {
+    CEcoLab1* pCMe = (CEcoLab1*)me;
+    IEcoInterfaceBus1* pIBus = 0;
+    int16_t result = -1;
+
+    /* Проверка указателей */
+    if (me == 0 ) {
+        return result;
+    }
+
+    /* Сохранение указателя на системный интерфейс */
+    pCMe->m_pISys = (IEcoSystem1*)pIUnkSystem;
+
+    /* Получение интерфейса для работы с интерфейсной шиной */
+    result = pCMe->m_pISys->pVTbl->QueryInterface(pCMe->m_pISys, &IID_IEcoInterfaceBus1, (void **)&pIBus);
+
+    /* Проверка указателей */
+    if (me == 0 ) {
+        return result;
+    }
+
+    /* Сохранение указателя на системный интерфейс */
+    pCMe->m_pISys = (IEcoSystem1*)pIUnkSystem;
+
+
+
+    /* Освобождение */
+    pIBus->pVTbl->Release(pIBus);
+
+    return result;
+}
+
+
+/*
+ * Вспомогательные функции для Stooge sort
+ */
+static void stoogeSortHelperInt(int* arr, int l, int r) {
+    if (arr[l] > arr[r]) {
+        int temp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = temp;
+    }
+    if (r - l + 1 > 2) {
+        int t = (r - l + 1) / 3;
+        stoogeSortHelperInt(arr, l, r - t);
+        stoogeSortHelperInt(arr, l + t, r);
+        stoogeSortHelperInt(arr, l, r - t);
+    }
+}
+
+static void stoogeSortHelperLong(long* arr, int l, int r) {
+    if (arr[l] > arr[r]) {
+        long temp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = temp;
+    }
+    if (r - l + 1 > 2) {
+        int t = (r - l + 1) / 3;
+        stoogeSortHelperLong(arr, l, r - t);
+        stoogeSortHelperLong(arr, l + t, r);
+        stoogeSortHelperLong(arr, l, r - t);
+    }
+}
+
+static void stoogeSortHelperFloat(float* arr, int l, int r) {
+    if (arr[l] > arr[r]) {
+        float temp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = temp;
+    }
+    if (r - l + 1 > 2) {
+        int t = (r - l + 1) / 3;
+        stoogeSortHelperFloat(arr, l, r - t);
+        stoogeSortHelperFloat(arr, l + t, r);
+        stoogeSortHelperFloat(arr, l, r - t);
+    }
+}
+
+static void stoogeSortHelperDouble(double* arr, int l, int r) {
+    if (arr[l] > arr[r]) {
+        double temp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = temp;
+    }
+    if (r - l + 1 > 2) {
+        int t = (r - l + 1) / 3;
+        stoogeSortHelperDouble(arr, l, r - t);
+        stoogeSortHelperDouble(arr, l + t, r);
+        stoogeSortHelperDouble(arr, l, r - t);
+    }
+}
+
+static void stoogeSortHelperLongDouble(long double* arr, int l, int r) {
+    if (arr[l] > arr[r]) {
+        long double temp = arr[l];
+        arr[l] = arr[r];
+        arr[r] = temp;
+    }
+    if (r - l + 1 > 2) {
+        int t = (r - l + 1) / 3;
+        stoogeSortHelperLongDouble(arr, l, r - t);
+        stoogeSortHelperLongDouble(arr, l + t, r);
+        stoogeSortHelperLongDouble(arr, l, r - t);
+    }
+}
+
+/*
+ *
+ * <сводка>
+ *   Функция stoogeSortInt
+ * </сводка>
+ *
+ * <описание>
+ *   Функция сортирует массив целых чисел с помощью алгоритма Stooge sort
+ * </описание>
+ *
+ */
+static int16_t ECOCALLMETHOD CEcoLab1_stoogeSortInt(/* in */ IEcoLab1Ptr_t me, /* in */ int* array, /* in */ int size) {
+	CEcoLab1* pCMe = (CEcoLab1*)me;
+    /* Проверка указателей */
+    if (me == 0 || array == 0 || size <= 0) {
+        return ERR_ECO_POINTER;
+    }
+    stoogeSortHelperInt(array, 0, size - 1);
+
+	return ERR_ECO_SUCCESES;
+}
+
+/*
+ *
+ * <сводка>
+ *   Функция stoogeSortLong
+ * </сводка>
+ *
+ * <описание>
+ *   Функция сортирует массив длинных целых чисел с помощью алгоритма Stooge sort
+ * </описание>
+ *
+ */
+static int16_t ECOCALLMETHOD CEcoLab1_stoogeSortLong(/* in */ IEcoLab1Ptr_t me, /* in */ long* array, /* in */ int size) {
+    CEcoLab1* pCMe = (CEcoLab1*)me;
+    /* Проверка указателей */
+    if (me == 0 || array == 0 || size <= 0) {
+        return ERR_ECO_POINTER;
+    }
+    stoogeSortHelperLong(array, 0, size - 1);
+
+	return ERR_ECO_SUCCESES;
+}
+
+/*
+ *
+ * <сводка>
+ *   Функция stoogeSortFloat
+ * </сводка>
+ *
+ * <описание>
+ *   Функция сортирует массив чисел с плавающей точкой с помощью алгоритма Stooge sort
+ * </описание>
+ *
+ */
+static int16_t ECOCALLMETHOD CEcoLab1_stoogeSortFloat(/* in */ IEcoLab1Ptr_t me, /* in */ float* array, /* in */ int size) {
+    CEcoLab1* pCMe = (CEcoLab1*)me;
+    /* Проверка указателей */
+    if (me == 0 || array == 0 || size <= 0) {
+       return ERR_ECO_POINTER;
+    }
+    stoogeSortHelperFloat(array, 0, size - 1);
+
+	return ERR_ECO_SUCCESES;
+}
+
+/*
+ *
+ * <сводка>
+ *   Функция stoogeSortDouble
+ * </сводка>
+ *
+ * <описание>
+ *   Функция сортирует массив чисел с плавающей точкой двойной точности с помощью алгоритма Stooge sort
+ * </описание>
+ *
+ */
+static int16_t ECOCALLMETHOD CEcoLab1_stoogeSortDouble(/* in */ IEcoLab1Ptr_t me, /* in */ double* array, /* in */ int size) {
+    CEcoLab1* pCMe = (CEcoLab1*)me;
+    /* Проверка указателей */
+    if (me == 0 || array == 0 || size <= 0) {
+        return ERR_ECO_POINTER;
+    }
+    stoogeSortHelperDouble(array, 0, size - 1);
+
+	return ERR_ECO_SUCCESES;
+}
+
+/*
+ *
+ * <сводка>
+ *   Функция stoogeSortLongDouble
+ * </сводка>
+ *
+ * <описание>
+ *   Функция сортирует массив чисел с плавающей точкой повышенной точности с помощью алгоритма Stooge sort
+ * </описание>
+ *
+ */
+static int16_t ECOCALLMETHOD CEcoLab1_stoogeSortLongDouble(/* in */ IEcoLab1Ptr_t me, /* in */ long double* array, /* in */ int size) {
+    CEcoLab1* pCMe = (CEcoLab1*)me;
+    /* Проверка указателей */
+    if (me == 0 || array == 0 || size <= 0) {
+        return ERR_ECO_POINTER;
+    }
+    stoogeSortHelperLongDouble(array, 0, size - 1);
+
+	return ERR_ECO_SUCCESES;
+}
+
+
+/* Create Virtual Table IEcoLab1 */
+IEcoLab1VTbl g_x277FC00C35624096AFCFC125B94EEC90VTbl = {
+    CEcoLab1_QueryInterface,
+    CEcoLab1_AddRef,
+    CEcoLab1_Release,
+	CEcoLab1_stoogeSortInt,
+	CEcoLab1_stoogeSortLong,
+	CEcoLab1_stoogeSortFloat,
+	CEcoLab1_stoogeSortDouble,
+	CEcoLab1_stoogeSortLongDouble
+};
+
+/*
+ *
+ * <сводка>
+ *   Функция Create
+ * </сводка>
+ *
+ * <описание>
+ *   Функция создания экземпляра
+ * </описание>
+ *
+ */
+int16_t ECOCALLMETHOD createCEcoLab1(/* in */ IEcoUnknown* pIUnkSystem, /* in */ IEcoUnknown* pIUnkOuter, /* out */ IEcoLab1** ppIEcoLab1) {
+    int16_t result = -1;
+    IEcoSystem1* pISys = 0;
+    IEcoInterfaceBus1* pIBus = 0;
+    IEcoInterfaceBus1MemExt* pIMemExt = 0;
+    IEcoMemoryAllocator1* pIMem = 0;
+    CEcoLab1* pCMe = 0;
+    UGUID* rcid = (UGUID*)&CID_EcoMemoryManager1;
+
+    /* Проверка указателей */
+    if (ppIEcoLab1 == 0 || pIUnkSystem == 0) {
+        return result;
+    }
+
+    /* Получение системного интерфейса приложения */
+    result = pIUnkSystem->pVTbl->QueryInterface(pIUnkSystem, &GID_IEcoSystem, (void **)&pISys);
+
+    /* Проверка */
+    if (result != 0 && pISys == 0) {
+        return result;
+    }
+
+    /* Получение интерфейса для работы с интерфейсной шиной */
+    result = pISys->pVTbl->QueryInterface(pISys, &IID_IEcoInterfaceBus1, (void **)&pIBus);
+
+    /* Получение идентификатора компонента для работы с памятью */
+    result = pIBus->pVTbl->QueryInterface(pIBus, &IID_IEcoInterfaceBus1MemExt, (void**)&pIMemExt);
+    if (result == 0 && pIMemExt != 0) {
+        rcid = (UGUID*)pIMemExt->pVTbl->get_Manager(pIMemExt);
+        pIMemExt->pVTbl->Release(pIMemExt);
+    }
+
+    /* Получение интерфейса распределителя памяти */
+    pIBus->pVTbl->QueryComponent(pIBus, rcid, 0, &IID_IEcoMemoryAllocator1, (void**) &pIMem);
+
+    /* Проверка */
+    if (result != 0 && pIMem == 0) {
+        /* Освобождение системного интерфейса в случае ошибки */
+        pISys->pVTbl->Release(pISys);
+        return result;
+    }
+
+    /* Выделение памяти для данных экземпляра */
+    pCMe = (CEcoLab1*)pIMem->pVTbl->Alloc(pIMem, sizeof(CEcoLab1));
+
+    /* Сохранение указателя на системный интерфейс */
+    pCMe->m_pISys = pISys;
+
+    /* Сохранение указателя на интерфейс для работы с памятью */
+    pCMe->m_pIMem = pIMem;
+
+    /* Установка счетчика ссылок на компонент */
+    pCMe->m_cRef = 1;
+
+    /* Создание таблицы функций интерфейса IEcoLab1 */
+    pCMe->m_pVTblIEcoLab1 = &g_x277FC00C35624096AFCFC125B94EEC90VTbl;
+
+    /* Инициализация данных */
+    pCMe->m_Name = 0;
+
+    /* Возврат указателя на интерфейс */
+    *ppIEcoLab1 = (IEcoLab1*)pCMe;
+
+    /* Освобождение */
+    pIBus->pVTbl->Release(pIBus);
+
+    return 0;
+}
+
+/*
+ *
+ * <сводка>
+ *   Функция Delete
+ * </сводка>
+ *
+ * <описание>
+ *   Функция освобождения экземпляра
+ * </описание>
+ *
+ */
+void ECOCALLMETHOD deleteCEcoLab1(/* in */ IEcoLab1* pIEcoLab1) {
+    CEcoLab1* pCMe = (CEcoLab1*)pIEcoLab1;
+    IEcoMemoryAllocator1* pIMem = 0;
+
+    if (pIEcoLab1 != 0 ) {
+        pIMem = pCMe->m_pIMem;
+        /* Освобождение */
+        if ( pCMe->m_Name != 0 ) {
+            pIMem->pVTbl->Free(pIMem, pCMe->m_Name);
+        }
+        if ( pCMe->m_pISys != 0 ) {
+            pCMe->m_pISys->pVTbl->Release(pCMe->m_pISys);
+        }
+        pIMem->pVTbl->Free(pIMem, pCMe);
+        pIMem->pVTbl->Release(pIMem);
+    }
+}
---
+++ UnitTestFiles/SourceFiles/EcoLab1.c
@@ -0,0 +1,537 @@
+﻿/*
+ * <кодировка символов>
+ *   Cyrillic (UTF-8 with signature) - Codepage 65001
+ * </кодировка символов>
+ *
+ * <сводка>
+ *   EcoLab1
+ * </сводка>
+ *
+ * <описание>
+ *   Данный исходный файл является точкой входа
+ * </описание>
+ *
+ * <автор>
+ *   Copyright (c) 2018 Vladimir Bashev. All rights reserved.
+ * </автор>
+ *
+ */
+
+
+/* Eco OS */
+#include "IEcoSystem1.h"
+#include "IdEcoMemoryManager1.h"
+#include "IdEcoInterfaceBus1.h"
+#include "IdEcoFileSystemManagement1.h"
+#include "IdEcoLab1.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Генерация случайного int в диапазоне [min, max]
+int rand_int(int min, int max) {
+    return min + rand() % (max - min + 1);
+}
+
+// Генерация случайного long в диапазоне [min, max]
+long rand_long(long min, long max) {
+    return min + rand() % (max - min + 1);
+}
+
+// Генерация случайного float в диапазоне [min, max]
+float rand_float(float min, float max) {
+    float scale = (float)rand() / (float)RAND_MAX;
+    return min + scale * (max - min);
+}
+
+// Генерация случайного double в диапазоне [min, max]
+double rand_double(double min, double max) {
+    double scale = (double)rand() / (double)RAND_MAX;
+    return min + scale * (max - min);
+}
+
+// Генерация случайного long double в диапазоне [min, max]
+long double rand_long_double(long double min, long double max) {
+    long double scale = (long double)rand() / (long double)RAND_MAX;
+    return min + scale * (max - min);
+}
+/*
+ *
+ * <сводка>
+ *   Функция EcoMain
+ * </сводка>
+ *
+ * <описание>
+ *   Функция EcoMain - точка входа
+ * </описание>
+ *
+ */
+int16_t EcoMain(IEcoUnknown* pIUnk) {
+    int16_t result = -1;
+    /* Указатель на системный интерфейс */
+    IEcoSystem1* pISys = 0;
+    /* Указатель на интерфейс работы с системной интерфейсной шиной */
+    IEcoInterfaceBus1* pIBus = 0;
+    /* Указатель на интерфейс работы с памятью */
+    IEcoMemoryAllocator1* pIMem = 0;
+    /* Указатель на тестируемый интерфейс */
+    IEcoLab1* pIEcoLab1 = 0;
+
+	int32_t* testIntArray = 0;
+	long* testLongArray = 0;
+	float* testFloatArray = 0;
+	double* testDoubleArray = 0;
+	long double* testLongDoubleArray = 0;
+	uint32_t printSize = 20;
+	uint32_t smallSize = 100;
+	uint32_t mediumSize = 500;
+	uint32_t largeSize = 1000;
+	uint32_t i = 0;
+
+	clock_t begin;
+    clock_t end;
+
+    srand(time(0));
+
+    /* Проверка и создание системного интрефейса */
+    if (pISys == 0) {
+        result = pIUnk->pVTbl->QueryInterface(pIUnk, &GID_IEcoSystem, (void **)&pISys);
+        if (result != 0 && pISys == 0) {
+        /* Освобождение системного интерфейса в случае ошибки */
+            goto Release;
+        }
+    }
+
+    /* Получение интерфейса для работы с интерфейсной шиной */
+    result = pISys->pVTbl->QueryInterface(pISys, &IID_IEcoInterfaceBus1, (void **)&pIBus);
+    if (result != 0 || pIBus == 0) {
+        /* Освобождение в случае ошибки */
+        goto Release;
+    }
+#ifdef ECO_LIB
+    /* Регистрация статического компонента для работы со списком */
+    result = pIBus->pVTbl->RegisterComponent(pIBus, &CID_EcoLab1, (IEcoUnknown*)GetIEcoComponentFactoryPtr_1F5DF16EE1BF43B999A434ED38FE8F3A);
+    if (result != 0 ) {
+        /* Освобождение в случае ошибки */
+        goto Release;
+    }
+#endif
+    /* Получение интерфейса управления памятью */
+    result = pIBus->pVTbl->QueryComponent(pIBus, &CID_EcoMemoryManager1, 0, &IID_IEcoMemoryAllocator1, (void**) &pIMem);
+
+    /* Проверка */
+    if (result != 0 || pIMem == 0) {
+        /* Освобождение системного интерфейса в случае ошибки */
+        goto Release;
+    }
+
+    /* Получение тестируемого интерфейса */
+    result = pIBus->pVTbl->QueryComponent(pIBus, &CID_EcoLab1, 0, &IID_IEcoLab1, (void**) &pIEcoLab1);
+    if (result != 0 || pIEcoLab1 == 0) {
+        /* Освобождение интерфейсов в случае ошибки */
+        goto Release;
+    }
+
+	printf("/// int32_t test ///\n");
+	{
+		{
+			printf("Test #1: ");
+			testIntArray = (int32_t*) pIMem->pVTbl->Alloc(pIMem, sizeof(int32_t) * smallSize);
+			for (i = 0; i < smallSize; ++i) {
+				testIntArray[i] = rand_int(-1000,1000);
+			}
+			for (i = 0; i < printSize; ++i) {
+				printf("%i ", testIntArray[i]);
+			}
+			printf("...\n");
+			begin = clock();
+			result = pIEcoLab1->pVTbl->stoogeSortInt(pIEcoLab1, testIntArray, smallSize); 
+			end = clock();
+			printf("Test took %f ms\n", (double) (end - begin) * 1000.0 / CLOCKS_PER_SEC);
+			printf("Result #1: ");
+			for (i = 0; i < printSize; ++i) {
+				printf("%i ", testIntArray[i]);
+			}
+			/* Освобождение блока памяти */
+			pIMem->pVTbl->Free(pIMem, testIntArray);
+
+			printf("...\n\n");
+		}
+
+		{
+			printf("Test #2: ");
+			testIntArray = (int32_t*) pIMem->pVTbl->Alloc(pIMem, sizeof(int32_t) * mediumSize);
+			for (i = 0; i < mediumSize; ++i) {
+				testIntArray[i] = rand_int(-1000,1000);
+			}
+			for (i = 0; i < printSize; ++i) {
+				printf("%i ", testIntArray[i]);
+			}
+			printf("...\n");
+			begin = clock();
+			result = pIEcoLab1->pVTbl->stoogeSortInt(pIEcoLab1, testIntArray, mediumSize); 
+			end = clock();
+			printf("Test took %f ms\n", (double) (end - begin) * 1000.0 / CLOCKS_PER_SEC);
+			printf("Result #2: ");
+			for (i = 0; i < printSize; ++i) {
+				printf("%i ", testIntArray[i]);
+			}
+			/* Освобождение блока памяти */
+			pIMem->pVTbl->Free(pIMem, testIntArray);
+
+			printf("...\n\n");
+		}
+
+		{
+			printf("Test #3: ");
+			testIntArray = (int32_t*) pIMem->pVTbl->Alloc(pIMem, sizeof(int32_t) * largeSize);
+			for (i = 0; i < largeSize; ++i) {
+				testIntArray[i] = rand_int(-1000,1000);
+			}
+			for (i = 0; i < printSize; ++i) {
+				printf("%i ", testIntArray[i]);
+			}
+			printf("...\n");
+			begin = clock();
+			result = pIEcoLab1->pVTbl->stoogeSortInt(pIEcoLab1, testIntArray, largeSize); 
+			end = clock();
+			printf("Test took %f ms\n", (double) (end - begin) * 1000.0 / CLOCKS_PER_SEC);
+			printf("Result #3: ");
+			for (i = 0; i < printSize; ++i) {
+				printf("%i ", testIntArray[i]);
+			}
+			/* Освобождение блока памяти */
+			pIMem->pVTbl->Free(pIMem, testIntArray);
+
+			printf("...\n\n");
+		}
+	}
+
+
+	printf("/// long test ///\n");
+	{
+		{
+			printf("Test #1: ");
+			testLongArray = (long*) pIMem->pVTbl->Alloc(pIMem, sizeof(long) * smallSize);
+			for (i = 0; i < smallSize; ++i) {
+				testLongArray[i] = rand_long(-1000,1000);
+			}
+			for (i = 0; i < printSize; ++i) {
+				printf("%ld ", testLongArray[i]);
+			}
+			printf("...\n");
+			begin = clock();
+			result = pIEcoLab1->pVTbl->stoogeSortLong(pIEcoLab1, testLongArray, smallSize); 
+			end = clock();
+			printf("Test took %f ms\n", (double) (end - begin) * 1000.0 / CLOCKS_PER_SEC);
+			printf("Result #1: ");
+			for (i = 0; i < printSize; ++i) {
+				printf("%ld ", testLongArray[i]);
+			}
+			/* Освобождение блока памяти */
+			pIMem->pVTbl->Free(pIMem, testLongArray);
+
+			printf("...\n\n");
+		}
+
+		{
+			printf("Test #2: ");
+			testLongArray = (long*) pIMem->pVTbl->Alloc(pIMem, sizeof(long) * mediumSize);
+			for (i = 0; i < mediumSize; ++i) {
+				testLongArray[i] = rand_long(-1000,1000);
+			}
+			for (i = 0; i < printSize; ++i) {
+				printf("%ld ", testLongArray[i]);
+			}
+			printf("...\n");
+			begin = clock();
+			result = pIEcoLab1->pVTbl->stoogeSortLong(pIEcoLab1, testLongArray, mediumSize); 
+			end = clock();
+			printf("Test took %f ms\n", (double) (end - begin) * 1000.0 / CLOCKS_PER_SEC);
+			printf("Result #2: ");
+			for (i = 0; i < printSize; ++i) {
+				printf("%ld ", testLongArray[i]);
+			}
+			/* Освобождение блока памяти */
+			pIMem->pVTbl->Free(pIMem, testLongArray);
+
+			printf("...\n\n");
+		}
+
+		{
+			printf("Test #3: ");
+			testLongArray = (long*) pIMem->pVTbl->Alloc(pIMem, sizeof(long) * largeSize);
+			for (i = 0; i < largeSize; ++i) {
+				testLongArray[i] = rand_long(-1000,1000);
+			}
+			for (i = 0; i < printSize; ++i) {
+				printf("%ld ", testLongArray[i]);
+			}
+			printf("...\n");
+			begin = clock();
+			result = pIEcoLab1->pVTbl->stoogeSortLong(pIEcoLab1, testLongArray, largeSize); 
+			end = clock();
+			printf("Test took %f ms\n", (double) (end - begin) * 1000.0 / CLOCKS_PER_SEC);
+			printf("Result #2: ");
+			for (i = 0; i < printSize; ++i) {
+				printf("%ld ", testLongArray[i]);
+			}
+			/* Освобождение блока памяти */
+			pIMem->pVTbl->Free(pIMem, testLongArray);
+
+			printf("...\n\n");
+		}
+	}
+
+	printf("/// float test ///\n");
+	{
+		{
+			printf("Test #1: ");
+			testFloatArray = (float*) pIMem->pVTbl->Alloc(pIMem, sizeof(float) * smallSize);
+			for (i = 0; i < smallSize; ++i) {
+				testFloatArray[i] = rand_float(-1000,1000);
+			}
+			for (i = 0; i < printSize; ++i) {
+				printf("%.2f ", testFloatArray[i]);
+			}
+			printf("...\n");
+			begin = clock();
+			result = pIEcoLab1->pVTbl->stoogeSortFloat(pIEcoLab1, testFloatArray, smallSize); 
+			end = clock();
+			printf("Test took %f ms\n", (double) (end - begin) * 1000.0 / CLOCKS_PER_SEC);
+			printf("Result #1: ");
+			for (i = 0; i < printSize; ++i) {
+				printf("%.2f ", testFloatArray[i]);
+			}
+			/* Освобождение блока памяти */
+			pIMem->pVTbl->Free(pIMem, testFloatArray);
+
+			printf("...\n\n");
+		}
+
+		{
+			printf("Test #2: ");
+			testFloatArray = (float*) pIMem->pVTbl->Alloc(pIMem, sizeof(float) * mediumSize);
+			for (i = 0; i < mediumSize; ++i) {
+				testFloatArray[i] = rand_float(-1000,1000);
+			}
+			for (i = 0; i < printSize; ++i) {
+				printf("%.2f ", testFloatArray[i]);
+			}
+			printf("...\n");
+			begin = clock();
+			result = pIEcoLab1->pVTbl->stoogeSortFloat(pIEcoLab1, testFloatArray, mediumSize); 
+			end = clock();
+			printf("Test took %f ms\n", (double) (end - begin) * 1000.0 / CLOCKS_PER_SEC);
+			printf("Result #2: ");
+			for (i = 0; i < printSize; ++i) {
+				printf("%.2f ", testFloatArray[i]);
+			}
+			/* Освобождение блока памяти */
+			pIMem->pVTbl->Free(pIMem, testFloatArray);
+
+			printf("...\n\n");
+		}
+
+		{
+			printf("Test #3: ");
+			testFloatArray = (float*) pIMem->pVTbl->Alloc(pIMem, sizeof(float) * largeSize);
+			for (i = 0; i < largeSize; ++i) {
+				testFloatArray[i] = rand_float(-1000,1000);
+			}
+			for (i = 0; i < printSize; ++i) {
+				printf("%.2f ", testFloatArray[i]);
+			}
+			printf("...\n");
+			begin = clock();
+			result = pIEcoLab1->pVTbl->stoogeSortFloat(pIEcoLab1, testFloatArray, largeSize); 
+			end = clock();
+			printf("Test took %f ms\n", (double) (end - begin) * 1000.0 / CLOCKS_PER_SEC);
+			printf("Result #2: ");
+			for (i = 0; i < printSize; ++i) {
+				printf("%.2f ", testFloatArray[i]);
+			}
+			/* Освобождение блока памяти */
+			pIMem->pVTbl->Free(pIMem, testFloatArray);
+
+			printf("...\n\n");
+		}
+	}
+
+	printf("/// double test ///\n");
+	{
+		{
+			printf("Test #1: ");
+			testDoubleArray = (double*) pIMem->pVTbl->Alloc(pIMem, sizeof(double) * smallSize);
+			for (i = 0; i < smallSize; ++i) {
+				testDoubleArray[i] = rand_double(-1000,1000);
+			}
+			for (i = 0; i < printSize; ++i) {
+				printf("%.2f ", testDoubleArray[i]);
+			}
+			printf("...\n");
+			begin = clock();
+			result = pIEcoLab1->pVTbl->stoogeSortDouble(pIEcoLab1, testDoubleArray, smallSize); 
+			end = clock();
+			printf("Test took %f ms\n", (double) (end - begin) * 1000.0 / CLOCKS_PER_SEC);
+			printf("Result #1: ");
+			for (i = 0; i < printSize; ++i) {
+				printf("%.2f ", testDoubleArray[i]);
+			}
+			/* Освобождение блока памяти */
+			pIMem->pVTbl->Free(pIMem, testDoubleArray);
+
+			printf("...\n\n");
+		}
+
+		{
+			printf("Test #2: ");
+			testDoubleArray = (double*) pIMem->pVTbl->Alloc(pIMem, sizeof(double) * mediumSize);
+			for (i = 0; i < mediumSize; ++i) {
+				testDoubleArray[i] = rand_double(-1000,1000);
+			}
+			for (i = 0; i < printSize; ++i) {
+				printf("%.2f ", testDoubleArray[i]);
+			}
+			printf("...\n");
+			begin = clock();
+			result = pIEcoLab1->pVTbl->stoogeSortDouble(pIEcoLab1, testDoubleArray, mediumSize); 
+			end = clock();
+			printf("Test took %f ms\n", (double) (end - begin) * 1000.0 / CLOCKS_PER_SEC);
+			printf("Result #2: ");
+			for (i = 0; i < printSize; ++i) {
+				printf("%.2f ", testDoubleArray[i]);
+			}
+			/* Освобождение блока памяти */
+			pIMem->pVTbl->Free(pIMem, testDoubleArray);
+
+			printf("...\n\n");
+		}
+
+		{
+			printf("Test #3: ");
+			testDoubleArray = (double*) pIMem->pVTbl->Alloc(pIMem, sizeof(double) * largeSize);
+			for (i = 0; i < largeSize; ++i) {
+				testDoubleArray[i] = rand_double(-1000,1000);
+			}
+			for (i = 0; i < printSize; ++i) {
+				printf("%.2f ", testDoubleArray[i]);
+			}
+			printf("...\n");
+			begin = clock();
+			result = pIEcoLab1->pVTbl->stoogeSortDouble(pIEcoLab1, testDoubleArray, largeSize); 
+			end = clock();
+			printf("Test took %f ms\n", (double) (end - begin) * 1000.0 / CLOCKS_PER_SEC);
+			printf("Result #2: ");
+			for (i = 0; i < printSize; ++i) {
+				printf("%.2f ", testDoubleArray[i]);
+			}
+			/* Освобождение блока памяти */
+			pIMem->pVTbl->Free(pIMem, testDoubleArray);
+
+			printf("...\n\n");
+		}
+	}
+	
+	printf("/// long double test ///\n");
+	{
+		{
+			printf("Test #1: ");
+			testLongDoubleArray = (long double*) pIMem->pVTbl->Alloc(pIMem, sizeof(long double) * smallSize);
+			for (i = 0; i < smallSize; ++i) {
+				testLongDoubleArray[i] = rand_long_double(-1000,1000);
+			}
+			for (i = 0; i < printSize; ++i) {
+				printf("%.3Lf ", testLongDoubleArray[i]);
+			}
+			printf("...\n");
+			begin = clock();
+			result = pIEcoLab1->pVTbl->stoogeSortLongDouble(pIEcoLab1, testLongDoubleArray, smallSize); 
+			end = clock();
+			printf("Test took %f ms\n", (double) (end - begin) * 1000.0 / CLOCKS_PER_SEC);
+			printf("Result #1: ");
+			for (i = 0; i < printSize; ++i) {
+				printf("%.3Lf ", testLongDoubleArray[i]);
+			}
+			/* Освобождение блока памяти */
+			pIMem->pVTbl->Free(pIMem, testLongDoubleArray);
+
+			printf("...\n\n");
+		}
+
+		{
+			printf("Test #2: ");
+			testLongDoubleArray = (long double*) pIMem->pVTbl->Alloc(pIMem, sizeof(long double) * mediumSize);
+			for (i = 0; i < mediumSize; ++i) {
+				testLongDoubleArray[i] = rand_long_double(-1000,1000);
+			}
+			for (i = 0; i < printSize; ++i) {
+				printf("%.3Lf ", testLongDoubleArray[i]);
+			}
+			printf("...\n");
+			begin = clock();
+			result = pIEcoLab1->pVTbl->stoogeSortLongDouble(pIEcoLab1, testLongDoubleArray, mediumSize); 
+			end = clock();
+			printf("Test took %f ms\n", (double) (end - begin) * 1000.0 / CLOCKS_PER_SEC);
+			printf("Result #2: ");
+			for (i = 0; i < printSize; ++i) {
+				printf("%.3Lf ", testLongDoubleArray[i]);
+			}
+			/* Освобождение блока памяти */
+			pIMem->pVTbl->Free(pIMem, testLongDoubleArray);
+
+			printf("...\n\n");
+		}
+
+		{
+			printf("Test #3: ");
+			testLongDoubleArray = (long double*) pIMem->pVTbl->Alloc(pIMem, sizeof(long double) * largeSize);
+			for (i = 0; i < largeSize; ++i) {
+				testLongDoubleArray[i] = rand_long_double(-1000,1000);
+			}
+			for (i = 0; i < printSize; ++i) {
+				printf("%.3Lf ", testLongDoubleArray[i]);
+			}
+			printf("...\n");
+			begin = clock();
+			result = pIEcoLab1->pVTbl->stoogeSortLongDouble(pIEcoLab1, testLongDoubleArray, largeSize); 
+			end = clock();
+			printf("Test took %f ms\n", (double) (end - begin) * 1000.0 / CLOCKS_PER_SEC);
+			printf("Result #2: ");
+			for (i = 0; i < printSize; ++i) {
+				printf("%.3Lf ", testLongDoubleArray[i]);
+			}
+			/* Освобождение блока памяти */
+			pIMem->pVTbl->Free(pIMem, testLongDoubleArray);
+
+			printf("...\n\n");
+		}
+	}
+
+Release:
+
+    /* Освобождение интерфейса для работы с интерфейсной шиной */
+    if (pIBus != 0) {
+        pIBus->pVTbl->Release(pIBus);
+    }
+
+    /* Освобождение интерфейса работы с памятью */
+    if (pIMem != 0) {
+        pIMem->pVTbl->Release(pIMem);
+    }
+
+    /* Освобождение тестируемого интерфейса */
+    if (pIEcoLab1 != 0) {
+        pIEcoLab1->pVTbl->Release(pIEcoLab1);
+    }
+
+
+    /* Освобождение системного интерфейса */
+    if (pISys != 0) {
+        pISys->pVTbl->Release(pISys);
+    }
+
+    return result;
+}
+
